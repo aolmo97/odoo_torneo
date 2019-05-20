38cfb53a024a6fdf16b99c9137cabb7ed09159e1
Antonio <aolmolopez97@gmail.com>
2019-05-20 22:26:14 +0200

    Subida

--- models/models.py
+++ models/models.py
@@ -9,13 +9,13 @@ class torneo(models.Model):
     categoria = fields.Selection([("Primera","Primera"),("Segunda","Segunda")],required=True)
     superficie = fields.Selection([("100m","100m"),("400m","400m")],required=True)
     fecha=fields.Date(string="Fecha",required=True)
-    tenistas=fields.One2many('torneo.tenista','nombre_tenista')
+    tenistas=fields.Many2many('torneo.tenista','nombre_tenista')
 class tenista(models.Model):
     _name = 'torneo.tenista'
     _rec_name= 'nombre_tenista'
     _sql_constraints=[("id_tenista",'UNIQUE(ranking)',"Error ya existe un tenista con ese ranking")]
     nombre_tenista = fields.Char(string="Nombre del tenista",required=True)
     ranking=fields.Integer(string="Numero del ranking",required=True)
-    torneos=fields.Many2one('torneo.torneo','nombre_torneo')
+    torneos=fields.Many2many('torneo.torneo','nombre_torneo')
     #fecha=fields.Date(string="Fecha",required=True)    
 
