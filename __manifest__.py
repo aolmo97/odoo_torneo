3ae95fe5887228243abfcedb8ee3144a36dab7ab
Antonio <aolmolopez97@gmail.com>
2019-05-20 21:49:25 +0200

    Subida

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/torneo.xml'
+        'views/torneo.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
